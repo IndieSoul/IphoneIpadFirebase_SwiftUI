c818657d665d7ada880d3563305e274adc51753c
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2023-05-07 16:32:07 -0600

    Initial Commit

---
+++ IphoneIpadFirebase/IphoneIpadFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneIpadFirebaseApp.swift
+//  IphoneIpadFirebase
+//
+//  Created by Luis Enrique Rosas Espinoza on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct IphoneIpadFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
